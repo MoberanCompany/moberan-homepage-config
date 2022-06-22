9163711d309079178132784f0384676dbd492649
bungabear <bungabear6422@gmail.com>
2022-06-22 22:50:52 +0900

    history 버그 수정 3

--- history.js
+++ history.js
@@ -86,7 +86,7 @@ function createYearHistory(isLeft, year, datas){
 function historyGenerate() {
     let elements = [];
     let keys = Object.keys(historyData);
-    for(let index; index < keys.length; index++){
+    for(let index = 0; index < keys.length; index++){
         let isLeft = index % 2;
         let key = keys[index];
         let value = historyData[key];
