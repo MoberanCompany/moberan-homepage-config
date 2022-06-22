74f8d51d94abff23ba323356491531a9099df3f9
bungabear <bungabear6422@gmail.com>
2022-06-22 22:08:59 +0900

    people 스크립트 수정

--- person/people.js
+++ person/people.js
@@ -1,15 +1,16 @@
-(function(){
+function peopleGenerate(){
     const data = [
         {
+            
             "name": "손민재",
-            "image":"https://github.com/moberancompany/moberan-homepage-config/master/persion/minjae.jfif", 
+            "image":"https://github.com/MoberanCompany/moberan-homepage-config/blob/main/person/minjae.jfif?raw=true", 
             "role": "Mobile Application",
             "bio":"전문가의 손길로 사용성 좋은 편리한 앱을 만들어 드리겠습니다!",
             "link":"https://github.com/bungabear"
         },
         {
             "name": "주해성",
-            "image":"https://github.com/moberancompany/moberan-homepage-config/master/persion/haesung.jfif", 
+            "image":"https://github.com/MoberanCompany/moberan-homepage-config/blob/main/person/haesung.jfif?raw=true", 
             "role": "Web Application",
             "bio":"사랑합니다 고객님❤",
             "link":"https://github.com/lululalala"
@@ -38,4 +39,10 @@
     
     let target = document.querySelector(".right_box > ul");
     target.innerHTML = divs.join('\n');
+}
+
+(function(){
+    document.addEventListener("DOMContentLoaded", () => {
+        peopleGenerate();
+    });
 })();
