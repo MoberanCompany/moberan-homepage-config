a16c0398acb4141bfc212f9a1f67f82f113a3fa0
haesungJoo <haesung0728@gmail.com>
2022-06-23 22:49:57 +0900

    회사 소개 설정 js 추가

---
+++ introduction.js
@@ -0,0 +1,70 @@
+const datas = [
+    {
+        "count": 5,
+        "isUnit": true,
+        "unit": "개",
+        "content": "방송사 납품"
+    },
+    {
+        "count": 5,
+        "isUnit": true,
+        "unit": "개",
+        "content": "방송사 납품"
+    },
+    {
+        "count": 5,
+        "isUnit": true,
+        "unit": "개",
+        "content": "방송사 납품"
+    },
+    {
+        "count": 5,
+        "isUnit": true,
+        "unit": "개",
+        "content": "방송사 납품"
+    },
+    {
+        "count": 5,
+        "isUnit": true,
+        "unit": "개",
+        "content": "방송사 납품"
+    },
+];
+
+function generateInformation({count, isUnit, unit, content}){
+    return `
+        <li>
+            <p class="count">${count}${isUnit ? `<span><${unit}/span>`: ""}</p>
+            <p class="ex_txt">${content}</p>
+        </li>
+    `;
+} 
+
+function generateCompanyInformationHtml(){
+
+    const datasArr = [];
+
+    datas.map((data)=>{
+        datasArr.push(
+            generateInformation(
+                {
+                    count: data.count,
+                    isUnit: data.isUnit,
+                    unit: data.unit,
+                    content: data.content,
+                }
+            )
+        )
+    });
+
+    return datasArr.join('\n');
+}
+
+(function(){
+    document.addEventListener("DOMContentLoaded", ()=>{
+        const companyInformationHtml = generateCompanyInformationHtml();
+        const informationDOM = document.querySelector("#section01 .about .about_list");
+
+        informationDOM.innerHTML(companyInformationHtml)
+    });
+})();
