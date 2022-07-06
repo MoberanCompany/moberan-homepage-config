f2ff2fa38b6cf0cc3da1dc1b1feba5d5ceec2711
bungabear <bungabear6422@gmail.com>
2022-07-06 15:03:39 +0900

    제품소개 버튼 없을때 라인수 늘림

--- product/product.js
+++ product/product.js
@@ -67,14 +67,17 @@ const productDatas = [
 
 const generateComponent = (data) => {
 
+    const isButtonEmpty = data.linkBtnGroup.length == 0;
+    const txtMaxLine = isButtonEmpty ? 5 : 3;
     newLineParsedContent = data.productContent.replaceAll("\n", "<br>");
+    const overrideTxtStyle = `-webkit-line-clamp: ${txtMaxLine} !important;`
 
     return `
     <li data-aos="fade-right" data-aos-offset="-400" data-aos-delay="200" data-aos-duration="800" data-aos-anchor-placement="top-center">
         ${data.badgeYN == "Y" ? `<div class="product_li_thumb" style="background-image: url('${data.thumbNailImagePath}'); background-size: cover; background-position: center;"><span>${data.badgeText}</span></div>`: `<div class="product_li_thumb" style="background-image: url('${data.thumbNailImagePath}'); background-size: cover; background-position: center;"></div>`}
         <div class="product_li_txt">
             <h3>${data.productTitle}</h3>
-            <p class="txt">${newLineParsedContent}</p>
+            <p class="txt" style="${overrideTxtStyle}">${newLineParsedContent}</p>
             <div class="btn_wrap">
                 ${generateBtnGroup(data.linkBtnGroup)}
             </div>
