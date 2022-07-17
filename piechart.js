3ff1103e692bb5d5ed2410709c61a05e6f84a38e
joohaesung <haesung0728@gmail.com>
2022-07-18 00:30:43 +0900

    [프로젝트 스펙트럼] 파이차트 이미지 태그 src 값 setting

--- piechart.js
+++ piechart.js
@@ -9,6 +9,6 @@
 (function(){
     window.onload = 
     document.addEventListener("onload", async ()=>{
-        document.querySelector('#section02 .pie-chart1 img').src = 'https://mwidget.moberan.com/api/widget/b8b329bf-42a3-4ca2-9783-7a8a65f87707'
+        document.querySelector('#section02 .pie-chart1 img').setAttribute('src', 'https://mwidget.moberan.com/api/widget/b8b329bf-42a3-4ca2-9783-7a8a65f87707')
     })
 }())
