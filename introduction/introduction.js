5fe660e1a748f083fb9139c505edaa3d433c4e61
Minjae Son <bungabear6422@gmail.com>
2023-03-02 13:08:34 +0900

    2023 회사 소개 위젯 적용

--- introduction/introduction.js
+++ introduction/introduction.js
@@ -1,23 +1,28 @@
 const datas = [
     {
         "url": "https://mwidget.moberan.com/api/widget/f5f7424d-4fce-4a74-8a5d-c650930232a8",
-        "asset": "broadcast.png"
+        "asset": "broadcast.png",
+        "memo": "방송사 납품"
     },
     {
-        "url": "https://mwidget.moberan.com/api/widget/422b2aa4-c979-4c45-8a35-2964fad083df",
-        "asset": "document.png"
+        "url": "https://mwidget.moberan.com/api/widget/84fd3be2-5e7d-4553-abbf-825f0b8662f6",
+        "asset": "document.png",
+        "memo": "연 프로젝트 완료"
     },
     {
         "url": "https://mwidget.moberan.com/api/widget/9c81b828-cbb9-41eb-8fa3-9b39757e3d6e",
-        "asset": "company.png"
+        "asset": "company.png",
+        "memo": "회사 창립"
     },
     {
-        "url": "https://mwidget.moberan.com/api/widget/10a445c3-9939-4406-8dac-f73ec66459f2",
-        "asset": "airplane.png"
+        "url": "https://mwidget.moberan.com/api/widget/600e57fa-c0e1-4202-aa64-5f6f0525555c",
+        "asset": "airplane.png",
+        "memo": "연 여행"
     },
     {
         "url": "https://mwidget.moberan.com/api/widget/23b97bc9-c346-46fe-81f3-52dee99035a5",
-        "asset": "idea.png"
+        "asset": "idea.png",
+        "memo": "Flutter 프로젝트 완료"
     },
 ];
 
