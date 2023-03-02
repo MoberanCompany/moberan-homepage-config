2e5d0055807c41d3dca75fd3f1f492031610e882
Minjae Son <bungabear6422@gmail.com>
2023-03-02 13:54:09 +0900

    제품 정보 업데이트
    * NFT 마켓 3건 -> 4건

--- product/product.js
+++ product/product.js
@@ -34,10 +34,10 @@ const productDatas = [
     },
     {
         badgeYN: "Y",
-        badgeText: "5개 프로젝트",
+        badgeText: "6개 프로젝트",
         thumbNailImagePath: "https://github.com/MoberanCompany/moberan-homepage-config/blob/main/product/asset/NFTMarket.png?raw=true",
         productTitle: "NFT 서비스 개발",
-        productContent: "\"블록체인 메인넷 개발 1건, NFT 마켓 3건, Dapp 개발 1건\"의 경험을 토대로\n블록체인 및 NFT 관련 서비스 개발에 적극 참여 가능합니다.",
+        productContent: "\"블록체인 메인넷 개발 1건, NFT 마켓 4건, Dapp 개발 1건\"의 경험을 토대로\n블록체인 및 NFT 관련 서비스 개발에 적극 참여 가능합니다.",
         linkBtnGroup: []
     },
 
